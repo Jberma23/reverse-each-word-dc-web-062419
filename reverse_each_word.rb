cfcd8d1a362d18eabb6155c949034db1a9612c8a
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-17 14:59:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
 array = string.split " "
 new_array = []
 array.each do |word|
-  new_array << array.reverse!
+  new_array << word.reverse!
 end
 new_array.join(" ")
 end
